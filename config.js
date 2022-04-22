981e73d29b512f7b95fcbd54ac94fca96671fbaa
HAD <103582402+hadbotz@users.noreply.github.com>
2022-04-22 07:10:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 global.DeveloperMode = 'false' //true atau false, true berati iya false berati tidak Moga paham
-global.owner = ['628818770766'] // masuk in no lu sebagai owner
-global.mods = ['6283897390164'] // masuk in no moderator ny
+global.owner = ['6285749433476'] // masuk in no lu sebagai owner
+global.mods = ['6285749433476] // masuk in no moderator ny
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // ini buat premium
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -30,15 +30,15 @@ global.APIKeys = { // Apikey lu di sini
 }
 
 // Sticker Watermark
-global.packname = 'NayLaBotz'
-global.author = 'Zevano'
+global.packname = 'HAD BOTZZ'
+global.author = 'HAD STORE'
 
 global.multiplier = 36 // ini butuh berapa xp buat levelup 
 
 global.wait = '*❒ WET TUNGGU*'
-global.error = '*❒ ERROR CUK MAAP*'
-global.botwm = '© nayla' // watermark bot
-global.oname = 'Zevano' // nama owner nya
+global.error = '*❒ ERROR COK MAAP*'
+global.botwm = '© hadbotzz' // watermark bot
+global.oname = 'HAD STORE' // nama owner nya
 global.thum = 'https://telegra.ph/file/5859353a380d9b67be198.jpg'
 
 let chalk = require('chalk')
